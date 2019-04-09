124bb9c67dc677ef0335cc5f8567bc8ccfd902ff
filmscore88 <cmtmorales24@gmail.com>
2019-04-09 10:24:12 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,42 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Mr. Chocolate", 10, "male", "black", "lazy",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Rowdy", 11, "male", "brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Tabitha", 7, "male", "brown", "astute", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Sergeant Brown", 5, "female", "blue", "intoxicated", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Melissa", 9, "female", "white", "albino", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Grinch", 11, "male", "green", "manipulative", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Wendy", 8, "female", "brown", "hyper", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES (NULL, 4, "male", "black", "unforgiving", 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
